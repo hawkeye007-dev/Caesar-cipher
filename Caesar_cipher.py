d92d89e6350fa685d1658636626844916c7ad99b
hawkeye007-dev <marriruthvik57@gmail.com>
2024-08-08 14:20:04 +0530

    Update Caesar_cipher.py

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -4,3 +4,19 @@ alphabets = string.ascii_lowercase
 direction = input("Type 'decode' to decode the script and 'encode' to code a script : \n").lower()
 text = input("Type the text you want to direct : \n").lower()
 shift = int(input("The shift number is : \n"))
+
+def output(text_0,shift_0,direction_0) :
+    cipher = ""
+    for letter in text_0 :
+        if letter not in alphabets :
+            cipher += letter
+        else :
+            index_0 = alphabets.index(letter)
+            if direction_0 == "decode" :
+                shift_0 = shift_0 * -1
+            new_index = index_0 + shift_0
+            new_index %= len(alphabets)
+            cipher += alphabets[new_index]
+    print(cipher)
+
+output(text,shift,direction)
